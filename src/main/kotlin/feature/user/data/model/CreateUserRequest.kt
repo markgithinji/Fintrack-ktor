a611d6a5517f8938aa31c41701fbb1f8eb6b9dcf
markgithinji <57723258+markgithinji@users.noreply.github.com>
2026-06-28 15:57:11 +0300

    feat(user): add @Serializable to user request models

--- src/main/kotlin/feature/user/data/model/CreateUserRequest.kt
+++ src/main/kotlin/feature/user/data/model/CreateUserRequest.kt
@@ -1,5 +1,8 @@
 package feature.user.data.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class CreateUserRequest(
     val email: String,
     val password: String
--- src/main/kotlin/feature/user/data/model/UpdateUserRequest.kt
+++ src/main/kotlin/feature/user/data/model/UpdateUserRequest.kt
@@ -1,5 +1,8 @@
 package feature.user.data.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class UpdateUserRequest(
     val name: String? = null,
     val email: String? = null,
